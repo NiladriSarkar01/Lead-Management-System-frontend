2fca060cadd694c0865dfa074616f082f5520d20
NiladriSarkar01 <niladrisarkar422@gmail.com>
2025-08-25 17:03:11 +0530

    Updated base url.

--- src/lib/axios.config.js
+++ src/lib/axios.config.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance=axios.create({
-    baseURL:'http://localhost:5001/api',
+    baseURL:'https://lead-management-system-ayqo.onrender.com',
     withCredentials:true
 })
